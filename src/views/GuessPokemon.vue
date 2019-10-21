e994c6f71f5bcb04d968adc612e3d1bae4112336
icvitkov <ivana.cvit@hotmail.com>
2019-10-21 19:48:14 +0200

    guessed pokemons do not apper in the game anymore

--- src/views/GuessPokemon.vue
+++ src/views/GuessPokemon.vue
@@ -20,7 +20,6 @@ export default {
 
   data() {
     return {
-      number: 0,
       pokemon: null,
       pokemonType: null,
       pokemonType2: null,
@@ -30,27 +29,31 @@ export default {
   },
 
   mounted() {
+    this.createPokemonList();
     this.randomPokemon();
-    this.createPokemonList(3);
   },
   methods: {
-    createPokemonList(max) {
+    createPokemonList() {
       let pokemonList = {};
+      let max = 3;
       for (let min = 1; min <= max; min++) {
         pokemonList[min] = {
           id: min
         };
       }
       this.$store.state.pokemonAll = pokemonList;
-     console.log("Pokemon broj: ", this.$store.state.pokemonAll[this.number].id);
+      console.log("id svih pokemona: ", this.$store.state.pokemonAll);
     },
 
     randomNumber() {
       let min = 1;
       let max = 3;
-      let random = Math.floor(Math.random() * (max - min + 1)) + min;
-      this.number = random;
-      return random;
+      let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+      if (this.$store.state.pokemonAll[randomNum].name) {
+        return this.randomNumber(); // recursive function 
+      } else {
+        return randomNum;
+      }
     },
 
     async randomPokemon() {
@@ -61,21 +64,22 @@ export default {
       this.pokemonType = pokemon.types[0].type.name;
       if (pokemon.types[1]) {
         this.pokemonType2 = pokemon.types[1].type.name;
-      } else{
-        this.pokemonType2= null;
+      } else {
+        this.pokemonType2 = null;
       }
     },
 
     checkAnswer() {
       if (this.answer.toLowerCase() === this.pokemon.name.toLowerCase()) {
-
         this.$store.state.pokemonAll[this.pokemon.id] = {
           name: this.pokemon.name,
           img: this.pokemon.sprites.front_default,
           stats: this.pokemon.stats
         };
-        console.log("točno," + this.$store.state.pokemonAll[this.number].name);
-         console.log("Pokemon broj: ", this.$store.state.pokemonAll);
+        console.log(
+          "točno," + this.$store.state.pokemonAll[this.pokemon.id].name
+        );
+        console.log("Pokemon broj: ", this.$store.state.pokemonAll);
         this.status = "točno";
         this.randomPokemon();
         this.answer = "";
@@ -91,7 +95,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 h3 {
   margin: 40px 0 0;
 }
@@ -107,57 +110,64 @@ a {
   color: #42b983;
 }
 
-.pokemon__img--container{
+.pokemon__img--container {
   display: grid;
-    width: 200px;
-    height: 200px;
-    padding: 10px;
-    justify-items: center;
-    margin: auto;
-    border-radius: 50%;
-    align-content: center;
-    position: relative;
+  width: 200px;
+  height: 200px;
+  padding: 10px;
+  justify-items: center;
+  margin: auto;
+  border-radius: 50%;
+  align-content: center;
+  position: relative;
 }
 
-.poison{
-   background: repeating-linear-gradient(rgb(95, 5, 65), rgb(87, 27, 182));
+.poison {
+  background: repeating-linear-gradient(rgb(95, 5, 65), rgb(87, 27, 182));
 }
-.ground{
-   background: repeating-linear-gradient(rgb(236, 130, 77), rgb(94, 63, 43));
+.ground {
+  background: repeating-linear-gradient(rgb(236, 130, 77), rgb(94, 63, 43));
 }
-.electric{
-   background: repeating-linear-gradient(rgb(230, 255, 1), rgb(41, 12, 145), rgb(16, 2, 99));
+.electric {
+  background: repeating-linear-gradient(
+    rgb(230, 255, 1),
+    rgb(41, 12, 145),
+    rgb(16, 2, 99)
+  );
 }
 /*   background: repeating-linear-gradient(rgb(0, 217, 255), rgb(255, 0, 140)); */
-.psychic{
-  background: repeating-linear-gradient(rgba(15, 4, 66, 0.788), rgb(255, 1, 192));
+.psychic {
+  background: repeating-linear-gradient(
+    rgba(15, 4, 66, 0.788),
+    rgb(255, 1, 192)
+  );
 }
-.bug{
-   background: repeating-linear-gradient(rgb(202, 64, 0), rgb(70, 148, 55));
+.bug {
+  background: repeating-linear-gradient(rgb(202, 64, 0), rgb(70, 148, 55));
 }
-.ice{
-   background: repeating-linear-gradient( rgb(255, 255, 255), rgb(70, 242, 248));
+.ice {
+  background: repeating-linear-gradient(rgb(255, 255, 255), rgb(70, 242, 248));
 }
 .water {
-   background: repeating-linear-gradient( rgb(0, 132, 255), rgb(8, 221, 186));
+  background: repeating-linear-gradient(rgb(0, 132, 255), rgb(8, 221, 186));
 }
 .grass {
-   background: repeating-linear-gradient( rgb(7, 80, 80), rgb(0, 202, 168));
+  background: repeating-linear-gradient(rgb(7, 80, 80), rgb(0, 202, 168));
 }
 .fire {
-  background: repeating-linear-gradient( rgb(255, 0, 0), rgb(248, 216, 70));
+  background: repeating-linear-gradient(rgb(255, 0, 0), rgb(248, 216, 70));
 }
-.normal{
-   background: repeating-linear-gradient(rgb(228, 136, 197),  rgb(81, 179, 236));
+.normal {
+  background: repeating-linear-gradient(rgb(228, 136, 197), rgb(81, 179, 236));
 }
-.fighting{
-   background: repeating-linear-gradient(rgb(2, 85, 105),  rgb(45, 21, 110));
+.fighting {
+  background: repeating-linear-gradient(rgb(2, 85, 105), rgb(45, 21, 110));
 }
-.fairy{
-   background: repeating-linear-gradient(rgb(252, 215, 172),  rgb(186, 111, 236));
+.fairy {
+  background: repeating-linear-gradient(rgb(252, 215, 172), rgb(186, 111, 236));
 }
 
-img{
+img {
   height: 200px;
   width: auto;
 }
