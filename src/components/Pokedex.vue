205f3542881ad31064b8f273d879b931b6284d88
icvitkov <ivana.cvit@hotmail.com>
2019-10-22 03:50:59 +0200

    pokedex component created

---
+++ src/components/Pokedex.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="pokedex">
+        <div class="pokedex__container">
+          <div class="pokemon__list">
+               <pokedexItem></pokedexItem>
+          </div>
+          <div class="pokemon__stats">Pokemon stats</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            bla: 'bla',
+        }
+    }
+}
+</script>
+<style scoped>
+.pokedex__container{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+.pokemon__list{
+    background: rgb(216, 69, 69);
+}
+.pokemon__stats{
+    background: rgb(76, 109, 216);
+}
+</style>
